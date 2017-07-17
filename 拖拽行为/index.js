ab3fc2685040ba8bb8bbd37f517a40775633a842
huanglaifeng <huanglaifeng@hexindai.com>
2017-07-17 17:11:35 +0800

    网页资源进行拖拽操作

--- 拖拽行为/index.js
+++ 拖拽行为/index.js
@@ -1,3 +1,37 @@
 /**
  * Created by huanglaifeng on 2017/7/17.
  */
+var box1Div,box2Div,msgDiv,img1;
+window.onload = function () {
+    console.log("大黄蜂");
+    box1Div = document.getElementById("box1");
+    box2Div = document.getElementById("box2");
+    msgDiv = document.getElementById("msg");
+    img1 = document.getElementById("img1");
+
+    box1Div.ondrop = dropImagHander;
+    box2Div.ondrop = dropImagHander;
+    box1Div.ondragover = function (e) {
+        e.preventDefault();
+    }
+    box2Div.ondragover = function (e) {
+        e.preventDefault();
+    }
+    img1.ondragstart = function (e) {
+        e.dataTransfer.setData("imgId","img1");
+    }
+
+}
+function dropImagHander(e) {
+    showObj(e.dataTransfer);
+    e.preventDefault();
+    var  img = document.getElementById(e.dataTransfer.getData("imgId"));
+    e.target.appendChild(img);
+}
+function showObj(obj) {
+    var  s = "";
+    for(var k in  obj){
+        s += k+":"+obj[k]+"<br/>"
+    }
+    msgDiv.innerHTML = s;
+}

8d7d0a59b6ae243839b57d5d15209564bb0ac71e
huanglaifeng <huanglaifeng@hexindai.com>
2017-07-31 15:34:09 +0800

    jQuery的简单使用

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,12 @@
 /**
  * Created by huanglaifeng on 2017/7/31.
  */
+$(document).ready(function () {
+    // alert("文档加载完毕");
+    $("p").click(function () {
+        $(this).hide();
+    });
+    $("button").click(function () {
+        $("#pid").text("p元素被修改了");
+    })
+});
